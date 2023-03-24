d2669b8b958747fb43bb084d2f1388d13a532381
Happy-Grass <3564494961@qq.com>
2023-03-25 03:27:00 +0800

    config for vimtex

---
+++ lua/modules/configs/tool/vimtex.lua
@@ -0,0 +1,7 @@
+return function()
+    --vim.g.vimtex_view_method = 'zathura'
+    --vim.g.vimtex_compiler_method = 'latexrun'
+    vim.g.vimtex_syntax_enabled = false
+    vim.o.conceallevel = 1
+    vim.g.tex_conceal = "abdmg"
+end
