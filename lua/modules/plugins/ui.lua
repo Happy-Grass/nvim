0f54211513620cee32df3462e371dfaf27c25c23
Happy-Grass <3564494961@qq.com>
2023-03-25 00:41:43 +0800

    add some ui plugins

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -6,4 +6,21 @@ ui["akinsho/bufferline.nvim"] = {
 	config = require("ui.bufferline"),
 }
 
+ui["nvim-lualine/lualine.nvim"] = {
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.lualine"),
+}
+
+ui["lewis6991/gitsigns.nvim"] = {
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+	config = require("ui.gitsigns"),
+}
+ui["lukas-reineke/indent-blankline.nvim"] = {
+	lazy = true,
+	event = "BufReadPost",
+	config = require("ui.indent-blankline"),
+}
+
 return ui
