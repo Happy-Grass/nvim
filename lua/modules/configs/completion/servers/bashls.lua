74483f53a772aee18071a77d82a53272e629064f
Happy-Grass <3564494961@qq.com>
2023-03-26 01:26:06 +0800

    bashls

--- lua/modules/configs/completion/servers/bashls.lua
+++ lua/modules/configs/completion/servers/bashls.lua
@@ -1,5 +1,9 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/bashls.lua
 return {
 	cmd = { "bash-language-server", "start" },
+	cmd_env = {
+		GLOB_PATTERN = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)",
+	},
+	root_dir = util.find_git_ancestor,
 	filetypes = { "bash", "sh" },
 }
