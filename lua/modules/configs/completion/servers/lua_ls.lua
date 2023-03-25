ca5f9712cc2aa9205d17c5e806248b9585c7ebed
Happy-Grass <3564494961@qq.com>
2023-03-26 01:26:10 +0800

    luals

--- lua/modules/configs/completion/servers/lua_ls.lua
+++ lua/modules/configs/completion/servers/lua_ls.lua
@@ -1,5 +1,30 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua
+local root_files = {
+	".luarc.json",
+	".luarc.jsonc",
+	".luacheckrc",
+	".stylua.toml",
+	"stylua.toml",
+	"selene.toml",
+	"selene.yml",
+}
+local util = require("lspconfig.util")
 return {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_dir = function(fname)
+		local root = util.root_pattern(unpack(root_files))(fname)
+		if root and root ~= vim.env.HOME then
+			return root
+		end
+		root = util.root_pattern("lua/")(fname)
+		if root then
+			return root .. "/lua/"
+		end
+		return util.find_git_ancestor(fname)
+	end,
+	single_file_support = true,
+	log_level = vim.lsp.protocol.MessageType.Warning,
 	settings = {
 		Lua = {
 			diagnostics = {
