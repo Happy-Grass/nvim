66a150508260d10084103c937b2ed8e473c5c31e
Happy-Grass <3564494961@qq.com>
2023-03-26 16:56:08 +0800

    modify the telescope

--- lua/modules/configs/tool/telescope.lua
+++ lua/modules/configs/tool/telescope.lua
@@ -1,47 +1,83 @@
 return function()
-	local telescope = require("telescope")
-	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-	vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	local icons = { ui = require("modules.utils.icons").get("ui", true) }
+	local lga_actions = require("telescope-live-grep-args.actions")
 
-	telescope.setup({
-		on_highlights = function(hl, c)
-			local prompt = "#2d3149"
-			hl.TelescopeNormal = {
-				bg = c.bg_dark,
-				fg = c.fg_dark,
-			}
-			hl.TelescopeBorder = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-			hl.TelescopePromptNormal = {
-				bg = prompt,
-			}
-			hl.TelescopePromptBorder = {
-				bg = prompt,
-				fg = prompt,
-			}
-			hl.TelescopePromptTitle = {
-				bg = prompt,
-				fg = prompt,
-			}
-			hl.TelescopePreviewTitle = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-			hl.TelescopeResultsTitle = {
-				bg = c.bg_dark,
-				fg = c.bg_dark,
-			}
-		end,
+	require("telescope").setup({
+		defaults = {
+			initial_mode = "insert",
+			prompt_prefix = " " .. icons.ui.Telescope .. " ",
+			selection_caret = icons.ui.ChevronRight,
+			scroll_strategy = "limit",
+			results_title = false,
+			layout_strategy = "horizontal",
+			path_display = { "absolute" },
+			file_ignore_patterns = { ".git/", ".cache", "%.class", "%.pdf", "%.mkv", "%.mp4", "%.zip" },
+			layout_config = {
+				horizontal = {
+					preview_width = 0.5,
+				},
+			},
+			file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+			grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+			qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+			file_sorter = require("telescope.sorters").get_fuzzy_file,
+			generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		},
+		pickers = {
+			keymaps = {
+				theme = "dropdown",
+			},
+		},
 		extensions = {
 			["ui-select"] = {
 				require("telescope.themes").get_dropdown({}),
 			},
+			fzf = {
+				fuzzy = false,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+			frecency = {
+				show_scores = true,
+				show_unindexed = true,
+				ignore_patterns = { "*.git/*", "*/tmp/*" },
+			},
+			live_grep_args = {
+				auto_quoting = true, -- enable/disable auto-quoting
+				-- define mappings, e.g.
+				mappings = { -- extend mappings
+					i = {
+						["<C-k>"] = lga_actions.quote_prompt(),
+						["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+					},
+				},
+			},
+			undo = {
+				side_by_side = true,
+				mappings = { -- this whole table is the default
+					i = {
+						-- IMPORTANT: Note that telescope-undo must be available when telescope is configured if
+						-- you want to use the following actions. This means installing as a dependency of
+						-- telescope in it's `requirements` and loading this extension from there instead of
+						-- having the separate plugin definition as outlined above. See issue #6.
+						["<cr>"] = require("telescope-undo.actions").yank_additions,
+						["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+						["<C-cr>"] = require("telescope-undo.actions").restore,
+					},
+				},
+			},
 		},
 	})
-	telescope.load_extension("ui-select")
+
+	require("telescope").load_extension("frecency")
+	require("telescope").load_extension("fzf")
+	require("telescope").load_extension("live_grep_args")
+	require("telescope").load_extension("notify")
+	-- require("telescope").load_extension("projects")
+	require("telescope").load_extension("undo")
+	require("telescope").load_extension("zoxide")
+	require("telescope").load_extension("neoclip")
+	require("telescope").load_extension("macroscope")
+	require("telescope").load_extension("ui-select")
 end
